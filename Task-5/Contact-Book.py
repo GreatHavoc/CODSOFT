736aa6f22c14dada37b37ca0bbad25a4461a4b1d
GreatHavoc <prakharsingh022official@gmail.com>
2023-08-15 21:30:23 +0530

    Database Updated

--- Task-5/Contact-Book.py
+++ Task-5/Contact-Book.py
@@ -27,10 +27,12 @@ def validate(n):
                 cur.execute("""INSERT INTO ContactDB('First_Name','Last_Name','Mobile_No','Mobile_id') VALUES (?,?,?,?)""",(fname.get(),lname.get(),mobile.get(),phoneid.get()))
                 con.commit()
                 messagebox.showinfo('Success','Contact Added Successfully')
+                cms.destroy()
             else:
                 cur.execute("""INSERT INTO ContactDB('First_Name','Last_Name','Mobile_No','Mobile_id','Email_Address','Email_Domain') VALUES (?,?,?,?,?,?)""",(fname.get(),lname.get(),mobile.get(),phoneid.get(),block1.get(),domain.get()))
                 con.commit()
                 messagebox.showinfo('Success','Contact Added Successfully')
+                cms.destroy()
         except(sqlite3.IntegrityError):
             messagebox.showerror('Integrity Error','Number Entered by You is Too Short or Too Long or Already Exist')
             mobile.delete(0,'end')
@@ -55,21 +57,21 @@ def show_details(event):
     selected=lis.curselection()
     simple=selected[0]
 
-    person_details=tb.Toplevel(title='Person Details',size=(350,400),position=(1000,300))
+    person_details=tb.Toplevel(title='Person Details',size=(350,280),position=(1100,300),resizable=(False,False))
     person_details.iconbitmap('contact.ico')
-
-    tb.Label(person_details,font=('Georgia',13),style='primary',text=data[simple][0]).place(x=172,y=2)
+    person_details.after(10000,person_details.destroy)
+    tb.Label(person_details,font=('Helevatica',13),style='primary',text=data[simple][0]).place(x=160,y=2)
     tb.Label(person_details,bootstyle='primary',font=('Georgia',12),text='Person Id').place(x=128,y=30)
 
     tb.Separator(person_details,bootstyle='info').place(x=0,y=57,width=350)
     tb.LabelFrame(person_details,text="First Name:",style='primary',height=60,width=160).grid(row=0,column=0,sticky='w',pady=(60,5),padx=(5,10))
-    tb.Label(person_details,text=data[simple][1],font=('',12),style='info').place(y=80,x=15)
+    tb.Label(person_details,text=data[simple][1],font=('Georgia',12),style='info').place(y=80,x=15)
     tb.LabelFrame(person_details,text="Last Name:",style='primary',height=60,width=160).grid(row=0,column=1,sticky='w',pady=(60,5),padx=(10,5))
     tb.Label(person_details,text=data[simple][2],font=('Georgia',12),style='info').place(y=80,x=200)
     tb.LabelFrame(person_details,bootstyle='primary',text='ISD code',height=60,width=113).place(x=8,y=130)
-    tb.Label(person_details,text=data[simple][4],font=('Georgia',12),style='info').place(x=12,y=150)
+    tb.Label(person_details,text=data[simple][4],font=('Helevatica',12),style='info').place(x=12,y=150)
     tb.LabelFrame(person_details,bootstyle='primary',text='Mobile No.',height=60,width=218).place(x=127,y=130)
-    tb.Label(person_details,text=data[simple][3],font=('Georgia',12),style=('info')).place(x=150,y=150)
+    tb.Label(person_details,text=data[simple][3],font=('Helevatica',12),style=('info')).place(x=175,y=150)
     tb.LabelFrame(person_details,text="Email Address",style='primary',height=60,width=337).grid(row=2,columnspan=2,pady=75)
     tb.Label(person_details,text=data[simple][5]+'@'+data[simple][6],font=('Georgia',10),style='info').place(x=12,y=220)
 
@@ -138,7 +140,7 @@ def update_window():
 
 def create_window():
 
-    global fname,lname,phoneid,mobile,block1,domain,block1
+    global fname,lname,phoneid,mobile,block1,domain,block1,cms
 
     cms=tb.Toplevel(title='Create Contact Page',size=(400,470),position=(980,250))
     cms.iconbitmap('contact.ico')
@@ -184,14 +186,24 @@ def create_window():
 
     cms.mainloop()
 
-
-
+def delete_contact():
+    selected=lis.curselection()
+    if(selected!=()):
+        simple=selected[0]
+        cur.execute("DELETE FROM ContactDB WHERE id= {} ".format(data[simple][0]))
+        con.commit()
+        del data[simple]
+        lis.delete(simple)
+        messagebox.showinfo('Success','Contact Deleted Successfully')
+    else:
+        messagebox.showerror('Selection Error','Please Select The Contact To Delete')
+    return
 root=tb.Window(themename='superhero',title='Contacts App',size=(500,600),position=(500,250),resizable=(0,0))
 root.iconbitmap('contact.ico')
 
 create=tb.Button(root,text='         + \nCreate New',style='success outline',command=create_window,width=12)
 update=tb.Button(root,text='          +- \nUpdate Existing',style='info outline',command=update_window)
-delete=tb.Button(root,text='          - \nDelete Contact',style='danger outline')
+delete=tb.Button(root,text='          - \nDelete Contact',style='danger outline',command=delete_contact)
 create.place(y=30,x=25)
 update.place(y=30,x=184)
 delete.place(y=30,x=350)
