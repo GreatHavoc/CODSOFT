e9ca26f17c1eb8b5a20bd8bf2c705d9f9bec2515
GreatHavoc <prakharsingh022official@gmail.com>
2023-08-15 20:03:52 +0530

    Day By Day Changes

--- Task-5/Contact-Book.py
+++ Task-5/Contact-Book.py
@@ -5,9 +5,10 @@ from ttkbootstrap.scrolled import ScrolledFrame
 from openpyxl import load_workbook
 import sqlite3
 
-
+#connection to database
 con=sqlite3.connect('contact_database.db')
 cur=con.cursor()
+#loading country codes from excel file
 wb=load_workbook('country.xlsx')
 ws=wb['Sheet1']
 cA=ws['A']
@@ -17,11 +18,46 @@ for i in range(len(cA)):
     countrylist.append((cA[i].value,cB[i].value))
 mail_list=["outlook.com","gmail.com","ymail.com","mail.com"]
 
+
+def validate(n):
+    global selected
+    if(n==0):
+        try:
+            if(block1.get()==''):
+                cur.execute("""INSERT INTO ContactDB('First_Name','Last_Name','Mobile_No','Mobile_id') VALUES (?,?,?,?)""",(fname.get(),lname.get(),mobile.get(),phoneid.get()))
+                con.commit()
+                messagebox.showinfo('Success','Contact Added Successfully')
+            else:
+                cur.execute("""INSERT INTO ContactDB('First_Name','Last_Name','Mobile_No','Mobile_id','Email_Address','Email_Domain') VALUES (?,?,?,?,?,?)""",(fname.get(),lname.get(),mobile.get(),phoneid.get(),block1.get(),domain.get()))
+                con.commit()
+                messagebox.showinfo('Success','Contact Added Successfully')
+        except(sqlite3.IntegrityError):
+            messagebox.showerror('Integrity Error','Number Entered by You is Too Short or Too Long or Already Exist')
+            mobile.delete(0,'end')
+    if(n==1):
+        simple=selected[0]
+        try:
+            if(block1u.get()==''):
+                cur.execute("""UPDATE ContactDB SET First_Name= ?,Last_Name= ?,Mobile_No= ?,Mobile_id= ? WHERE id= ? """,(fnameu.get(),lnameu.get(),mobileu.get(),phoneidu.get(),str(data[simple][0])))                
+                con.commit()
+                messagebox.showinfo('Success','Contact Updated Successfully')
+            else:
+                cur.execute("UPDATE ContactDB SET First_Name= ?,Last_Name= ?,Mobile_No= ?,Mobile_id= ?,Email_Address= ?,Email_Domain= ? WHERE id= ? """,(fnameu.get(),lnameu.get(),mobileu.get(),phoneidu.get(),block1u.get(),domainu.get(),str(data[simple][0])))
+                con.commit() 
+                messagebox.showinfo('Success','Contact Updated Successfully')
+        except(sqlite3.IntegrityError):
+            messagebox.showerror('Integrity Error','Number Entered by You is Too Short or Too Long or Already Exist')
+            mobile.delete(0,'end')
+    
+
 def show_details(event):
+    global selected
     selected=lis.curselection()
+    simple=selected[0]
+
     person_details=tb.Toplevel(title='Person Details',size=(350,400),position=(1000,300))
     person_details.iconbitmap('contact.ico')
-    simple=int(str(selected)[1])
+
     tb.Label(person_details,font=('Georgia',13),style='primary',text=data[simple][0]).place(x=172,y=2)
     tb.Label(person_details,bootstyle='primary',font=('Georgia',12),text='Person Id').place(x=128,y=30)
 
@@ -35,39 +71,75 @@ def show_details(event):
     tb.LabelFrame(person_details,bootstyle='primary',text='Mobile No.',height=60,width=218).place(x=127,y=130)
     tb.Label(person_details,text=data[simple][3],font=('Georgia',12),style=('info')).place(x=150,y=150)
     tb.LabelFrame(person_details,text="Email Address",style='primary',height=60,width=337).grid(row=2,columnspan=2,pady=75)
-    tb.Label(person_details,text=data[simple][5],font=('Georgia',10),style='info').place(x=12,y=220)
+    tb.Label(person_details,text=data[simple][5]+'@'+data[simple][6],font=('Georgia',10),style='info').place(x=12,y=220)
 
 
+ 
+def update_window():
+    global selected
+    selected=lis.curselection()
+    if(selected!=()):
+        simple=int(str(selected)[1])
+        ums=tb.Toplevel(title='Update Contact Page',size=(400,470),position=(780,250))
+        ums.iconbitmap('contact.ico')
 
-def dosomthing():
-    pass 
-def update_contact():
+        global fnameu,lnameu,phoneidu,mobileu,block1u,domainu,block1u
+        
+        tb.Label(ums,bootstyle='primary',font=('Georgia',12),text='Enter the contact details below: ').grid(row=0,padx=(6,0),pady=(10,0))
 
-    ums=tb.Toplevel(title='Create Contact Page',size=(400,470),position=(780,250))
-    ums.iconbitmap('contact.ico')
+        tb.Separator(ums,bootstyle='info-dotted').place(x=0,y=50,width=400)
 
-    ums.mainloop()
+        nameu=tb.LabelFrame(ums,bootstyle='info',text='Name: ',height=100,width=390)
+        nameu.grid(row=2,column=0,padx=(8,0),pady=(20,5),sticky='n,e,w,s')
+
+        tb.Label(nameu,text="First Name:",font=('Georgia',12),style='primary').grid(row=0,column=0,pady=(7,5),padx=10)
+
+        fnameu=tb.Entry(nameu,style='info',font=("Gerogia",12),justify='center')
+        fnameu.grid(row=0,column=1)
+        fnameu.insert(0,data[simple][1])
+        tb.Label(nameu,text="Last Name:",font=('Georgia',12),style='primary').grid(row=1,column=0,pady=10,padx=10)
+        lnameu=tb.Entry(nameu,style='info',font=("Gerogia",12),justify='center')
+        lnameu.grid(row=1,column=1)
+        lnameu.insert(0,data[simple][2])
+
+        phoneu=tb.LabelFrame(ums,bootstyle='info',text='Phone No.: ',height=100,width=390)
+        phoneu.grid(row=3,column=0,padx=(8,0),pady=5,sticky='n,e,w,s')
+        tb.Label(phoneu,text="Select Country:",font=('Georgia',12),style='primary').grid(row=0,column=0,pady=10,padx=10,sticky='w')
+
+        phoneidu=tb.Combobox(phoneu,style='info',values=countrylist)
+        phoneidu.grid(row=0,column=1,sticky='w',padx=10,pady=10)
+        phoneidu.set(data[simple][4])
+
+        tb.Label(phoneu,text="Mobile No.:",font=('Georgia',11),style='primary').grid(row=1,column=0,pady=10,padx=10,sticky='w')
+        mobileu=tb.Entry(phoneu,style='info',font=("Gerogia",11),width=18)
+        mobileu.grid(row=1,column=1,padx=10,pady=10)
+        mobileu.insert(0,data[simple][3])
+
+        emailu=tb.LabelFrame(ums,bootstyle='info',text='Email: ',height=100,width=390)
+        emailu.grid(row=4,column=0,padx=(8,0),pady=5,sticky='n,e,w,s')
+        block1u=tb.Entry(emailu,style='primary',font=('Georgia',11),width=18)
+        block1u.grid(row=0,column=0,padx=(5,3),pady=10,sticky='w')
+        block1u.insert(0,data[simple][5])
+
+        tb.Label(emailu,text='@',font=('Georgia',11),style='primary').grid(row=0,column=1,padx=3,pady=(0,7))
+        domainu=tb.Combobox(emailu,style='info',values=mail_list,width=11)
+        domainu.grid(row=0,column=2,padx=4,pady=10)
+        domainu.set(data[simple][6])
+
+        update=tb.Button(ums,style='danger,outline',text='            +\nUpdate Contact',command=lambda:validate(1))
+        update.grid(row=5,padx=50,pady=7)
+
+
+        ums.mainloop()
+    else:
+        messagebox.showerror('Selection Error','Please select one Contact from the list by clicking it.')
 
     
-def validate():
-    con=sqlite3.connect('contact_database.db')
-    cur=con.cursor()
-    email_address=str(block1.get())+'@'+str(domain.get())
-    try:
-        if(email_address[0]=='@'):
-            cur.execute("""INSERT INTO ContactDB('First_Name','Last_Name','Mobile_No','Mobile_id') VALUES (?,?,?,?)""",(fname.get(),lname.get(),mobile.get(),phoneid.get()))
-            messagebox.showinfo('Success','Contact Added Successfully')
-        else:
-            cur.execute("""INSERT INTO ContactDB('First_Name','Last_Name','Mobile_No','Mobile_id','Email_address') VALUES (?,?,?,?,?)""",(fname.get(),lname.get(),mobile.get(),phoneid.get(),email_address))
-            messagebox.showinfo('Success','Contact Added Successfully')
-    except(sqlite3.IntegrityError):
-        messagebox.showerror('Integrity Error','Number Entered by You is Too Short or Too Long or Already Exist')
-        mobile.delete(0,'end')
-    con.commit() 
-
 
 def create_window():
-    global fname,lname,phoneid,mobile,block1,domain
+
+    global fname,lname,phoneid,mobile,block1,domain,block1
+
     cms=tb.Toplevel(title='Create Contact Page',size=(400,470),position=(980,250))
     cms.iconbitmap('contact.ico')
 
@@ -102,21 +174,23 @@ def create_window():
     email.grid(row=4,column=0,padx=(8,0),pady=5,sticky='n,e,w,s')
     block1=tb.Entry(email,style='primary',font=('Georgia',11),width=18)
     block1.grid(row=0,column=0,padx=(5,3),pady=10,sticky='w')
-    at=tb.Label(email,text='@',font=('Georgia',11),style='primary').grid(row=0,column=1,padx=3,pady=(0,7))
+    tb.Label(email,text='@',font=('Georgia',11),style='primary').grid(row=0,column=1,padx=3,pady=(0,7))
     domain=tb.Combobox(email,style='info',values=mail_list,width=11)
     domain.grid(row=0,column=2,padx=4,pady=10)
     domain.current(1)
 
-    add=tb.Button(cms,style='danger,outline',text='         +\nAdd Contact',command=validate)
+    add=tb.Button(cms,style='danger,outline',text='         +\nAdd Contact',command=lambda:validate(0))
     add.grid(row=5,padx=50,pady=7)
 
     cms.mainloop()
+
+
 
 root=tb.Window(themename='superhero',title='Contacts App',size=(500,600),position=(500,250),resizable=(0,0))
 root.iconbitmap('contact.ico')
 
 create=tb.Button(root,text='         + \nCreate New',style='success outline',command=create_window,width=12)
-update=tb.Button(root,text='          +- \nUpdate Existing',style='info outline')
+update=tb.Button(root,text='          +- \nUpdate Existing',style='info outline',command=update_window)
 delete=tb.Button(root,text='          - \nDelete Contact',style='danger outline')
 create.place(y=30,x=25)
 update.place(y=30,x=184)
@@ -138,11 +212,12 @@ sf.pack(pady=(175,4),expand=t.NO,padx=5)
 lis=t.Listbox(sf,height=14,font=('Georgia',14),justify='center',highlightcolor='blue')
 lis.pack(expand=t.NO,fill=t.BOTH)
 cur.execute('SELECT * FROM ContactDB')
-global data
 data=cur.fetchall()
 count=0
 for i in data:
     lis.insert(count,i[1] +' '+i[2])
     count+=1
 lis.bind('<Double-1>',show_details)
+
+
 root.mainloop()
